f8cf3a1eac63d8d838745c38e7960a589fbe3dde
kuramnandhu <kuramnandhu@gmail.com>
2025-09-14 14:39:40 +0530

    Create solver.py

---
+++ solver.py
@@ -0,0 +1,35 @@
+# solver.py
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    row, col = pos
+    for j in range(len(board[0])):
+        if board[row][j] == num and j != col:
+            return False
+    for i in range(len(board)):
+        if board[i][col] == num and i != row:
+            return False
+    box_x, box_y = col // 3, row // 3
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    row, col = find
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+            if solve(board):
+                return True
+            board[row][col] = 0
+    return False
